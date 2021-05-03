8b99528560ce73a83a847474a0ad182442fc53bc
prestonlammy <77915689+prestonlammy@users.noreply.github.com>
2021-05-03 14:24:32 +0800

    Add files via upload

---
+++ personal page/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if(this.scrollY > 2){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    
+    }
+    
+    )
+
+
+
+})
+
+
+
